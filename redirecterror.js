81f8e5674a89ba19add2fdcedc5d5ad13a5bc464
BannerBomb <BannerBomb55@gmail.com>
2016-03-10 21:23:11 -0500

    Update redirecterror.js

--- redirecterror.js
+++ redirecterror.js
@@ -1,5 +1,5 @@
 //similar behavior as an HTTP redirect
-var RedirectError = true;
+var RedirectError = false;
 
 if (RedirectError) {
   window.location.replace("http://freeswfgames.github.io/redirecterror.html");
